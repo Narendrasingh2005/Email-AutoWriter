24f08406a11d142dab7528f2f490b1f702ef7790
Narendra Singh <ns1039492@gmail.com>
2025-05-09 23:50:35 +0530

    frontend update

--- email-writer-react/src/App.jsx
+++ email-writer-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { TextField } from '@mui/material';
+import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
@@ -11,6 +11,10 @@ function App() {
   const[generatedReply,setGeneratedReply] = useState('');
   const[loading,setLoading] = useState('');
   const[error,setError] = useState('');
+
+  const handleSubmit = async () =>{
+
+  };
 
   return (
       <Container maxWidth='md' sx={{py:4}}>
@@ -26,7 +30,36 @@ function App() {
             onChange={(e)=>setEmailContent(e.target.value)}
             sx = {{mb:2}}
             />
+
+
+          <FormControl fullWidth sx={{ mb:2 }}>
+            <InputLabel>Tone (Optional)</InputLabel>
+            <Select
+              value={tone || ''}
+              label={"Tone (Optional)"}
+              onChange={(e) => setTone(e.target.value)}>
+                <MenuItem value="">None</MenuItem>
+                <MenuItem value="professional">Professional</MenuItem>
+                <MenuItem value="casual">Casual</MenuItem>
+                <MenuItem value="friendly">Friendly</MenuItem>
+            </Select>
+          </FormControl>
+
+          <Button
+            variant='contained'
+            onClick={handleSubmit}
+            disabled={!emailContent || loading}
+            fullWidth>
+            {loading ? <CircularProgress size={24}/> : "Generate Reply"}
+          </Button>
+
         </Box>
+
+        {error && (
+          <Typography color='error' sx={{mb:2}}>
+            {error}
+          </Typography>
+        )}
 
       </Container>
   )
